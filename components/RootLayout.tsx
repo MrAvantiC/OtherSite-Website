14bf2e6db9bdfe9335fa3dae7dc374490cc52890
René Mäkeler <rene.maekeler@othersite.io>
2025-01-09 23:22:00 +0100

    Remove page navigation for now

--- components/RootLayout.tsx
+++ components/RootLayout.tsx
@@ -88,18 +88,20 @@ function NavigationItem({ href, children }: { href: string; children: React.Reac
 }
 
 function Navigation() {
-  return (
-    <nav className="mt-px font-display text-5xl font-medium tracking-tight text-white">
-      <NavigationRow>
-        <NavigationItem href="/work">Our Work</NavigationItem>
-        <NavigationItem href="/about">About Us</NavigationItem>
-      </NavigationRow>
-      <NavigationRow>
-        <NavigationItem href="/process">Our Process</NavigationItem>
-        <NavigationItem href="/blog">Blog</NavigationItem>
-      </NavigationRow>
-    </nav>
-  )
+  return null
+
+  // return (
+  //   <nav className="mt-px font-display text-5xl font-medium tracking-tight text-white">
+  //     <NavigationRow>
+  //       <NavigationItem href="/work">Our Work</NavigationItem>
+  //       <NavigationItem href="/about">About Us</NavigationItem>
+  //     </NavigationRow>
+  //     <NavigationRow>
+  //       <NavigationItem href="/process">Our Process</NavigationItem>
+  //       <NavigationItem href="/blog">Blog</NavigationItem>
+  //     </NavigationRow>
+  //   </nav>
+  // )
 }
 
 function RootLayoutInner({ children }: { children: React.ReactNode }) {
