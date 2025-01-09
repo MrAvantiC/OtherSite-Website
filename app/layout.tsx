29f5efde8fef383be5d457ae4111585d1c1ad6b0
René Mäkeler <rene.maekeler@othersite.io>
2025-01-09 21:18:21 +0100

    Add vercel analytics plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from 'next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
 
 import { RootLayout } from '@/components/RootLayout'
 
@@ -19,6 +20,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
 
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
