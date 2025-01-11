57621e5429d2f982ead654e54440e7c1803c00ca
René Mäkeler <rene.maekeler@othersite.io>
2025-01-12 00:02:50 +0100

    Remove vercel packages after switching hosters

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import { type Metadata } from 'next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import { Analytics } from '@vercel/analytics/next'
 
 import { RootLayout } from '@/components/RootLayout'
 
@@ -33,9 +31,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
-
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   )
